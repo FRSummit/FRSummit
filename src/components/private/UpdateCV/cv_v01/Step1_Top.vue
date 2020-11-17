76dfcafe018d6271264ad6cbf384afebeeade7ba
F R Summit <www.frsummit1@gmail.com>
2020-11-17 16:36:49 +0600

    update name sectin design fixed

--- src/components/private/UpdateCV/cv_v01/Step1_Top.vue
+++ src/components/private/UpdateCV/cv_v01/Step1_Top.vue
@@ -1,5 +1,120 @@
 <template>
-    <div class="update-name-section">
-        Name Section
-    </div>
+  <div class="update-name-section">
+    <p class="title-text">Update Name, Address &amp; Image</p>
+    <form @submit.prevent="onSubmit">
+      <div class="form-innse-section">
+        <label class="label">Name </label>
+        <input class="input" type="text" v-model="info1" id="info1" />
+      </div>
+      <div class="form-innse-section">
+        <label class="label" for="lname">Address </label>
+        <textarea class="textarea" type="text" v-model="info2" id="info2" />
+      </div>
+      <div class="form-innse-section">
+        <label class="label" for="lname">Email </label>
+        <input class="input" type="text" v-model="info3" id="info3" />
+      </div>
+      <div class="form-innse-section">
+        <label class="label" for="lname">Phone </label>
+        <input class="input" type="text" v-model="info4" id="info4" />
+      </div>
+      <div class="btn-section">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      info1: null,
+      info2: null,
+      info3: null,
+      info4: null,
+      info5: null,
+      info6: null,
+      info7: null,
+      leagueInfo: [],
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("SUbmitted");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.update-name-section {
+  background: #a8fda8;
+  position: absolute;
+  top: 0;
+  right: -502px;
+  z-index: 10;
+  width: 500px;
+  border: 2px solid #272727;
+  border-radius: 6px;
+  padding: 6px;
+}
+.update-name-section .title-text {
+  color: #dafff6;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+.update-name-section .form-innse-section {
+  margin-bottom: 10px;
+}
+.update-name-section .form-innse-section:last-child {
+  margin-bottom: 0px;
+}
+.update-name-section .label {
+  display: inline-block;
+  width: 20%;
+  vertical-align: top;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 4px 0;
+  color: #dafff6;
+  vertical-align: middle;
+}
+.update-name-section .input,
+.update-name-section .textarea {
+  display: inline-block;
+  width: 80%;
+  border-radius: 6px;
+  border: 2px solid #dafff6;
+  color: #dafff6;
+  padding: 6px 14px;
+  vertical-align: middle;
+}
+.update-name-section .textarea {
+  height: 80px;
+}
+.update-name-section .btn-section {
+  width: 100%;
+  text-align: center;
+  margin: 10px 0;
+}
+.update-name-section button {
+  border: 2px solid #272727;
+  border-radius: 6px;
+  background: #272727;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 16px;
+  padding: 4px 10px;
+}
+.delete-btn {
+  text-decoration: none;
+  font-weight: bold;
+  color: #ffffff;
+  border: 1px solid #272727;
+  border-radius: 4px;
+  background-color: red;
+  width: 15%;
+  vertical-align: middle;
+}
+</style>
--- src/views/private/UpdateCV/cv_v01/Update_CV.vue
+++ src/views/private/UpdateCV/cv_v01/Update_CV.vue
@@ -1,14 +1,101 @@
 <template>
-    <div class="update-cv">
-        Updates
+  <div class="update-cv">
+    <div class="update-cv-inner">
+      <div class="update-top-name-section">
+        <button
+          class="update-btn"
+          @click="updateClickHandler('TOP_NAME_SECTION')"
+          :class="topNameSection ? 'update-btn-close' : ''"
+        >
+          {{ topNameSectionBtnText }}
+        </button>
+        <Step1Top />
+        <UpdateStep1Top v-if="topNameSection" class="modal" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Step1Top from "../../../../components/public/cv/cv_v01/Step1_Top";
+import UpdateStep1Top from "../../../../components/private/UpdateCV/cv_v01/Step1_Top";
 
 export default {
-}
+  name: "UpdateCV",
+  components: {
+    Step1Top,
+    UpdateStep1Top,
+  },
+  data() {
+    return {
+      topNameSectionBtnText: "Update",
+      topNameSection: false,
+    };
+  },
+  methods: {
+    updateClickHandler(sectionName) {
+      console.log(sectionName);
+      if (this.topNameSection) {
+        this.topNameSectionBtnText = "Update";
+        this.topNameSection = false;
+      } else {
+        this.topNameSectionBtnText = "Close";
+        this.topNameSection = true;
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
+.update-cv {
+  background: #272727a3;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  color: #222222;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.update-cv-inner {
+  background: #fffff4;
+  width: 680px;
+  margin: 40px auto;
+  box-shadow: 0 1px 20px 4px #222222;
+  padding: 60px 46px 50px 94px;
+  /* overflow: hidden; */
+  overflow: auto;
+}
+.update-top-name-section {
+  position: relative;
+  border: 1px solid #27272759;
+  padding: 0 4px;
+}
+.update-btn {
+  position: absolute;
+  left: -80px;
+  background-color: #272727;
+  border-radius: 2px;
+  font-weight: 700;
+  border: 0;
+  cursor: pointer;
+  color: #ffffff;
+  padding: 8px 16px;
+  font-size: 14px;
+}
+.update-btn-close {
+  background-color: red !important;
+}
+/* Modal */
+.modal {
+  position: fixed;
+  z-index: 1;
+  padding: 20px 12px 0 0;
+  left: 27%;
+  top: 100px;
+  width: 50%;
+  overflow: auto;
+  background-color: rgb(0 0 0 / 70%);
+}
 </style>
